14b97d498ce26b02a7bf0ffd0cc52962405d72a1
Griffin Taylor <71144499+Zander-9909@users.noreply.github.com>
2023-06-19 12:34:16 -0400

    More testing

--- imagerec.py
+++ imagerec.py
@@ -2,16 +2,46 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import sys
+import imutils
 
-img_rgb = cv2.imread("C:\Users\Megan-2\Documents\GitHub\ReplayAnalysis\SSforOpenCV.PNG")
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread("C:\Users\Megan-2\Documents\GitHub\ReplayAnalysis\sledge.png",0)
-w, h = template.shape[::-1]
+imagePath = r'C:/Users/Megan-2/Documents/GitHub/ReplayAnalysis/SSforOpenCV.PNG'
 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+template = cv2.imread(r'C:/Users/Megan-2/Documents/GitHub/ReplayAnalysis/sledgeSmall.png')
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+template = cv2.Canny(template, 50, 200)
+(tH, tW) = template.shape[:2]
+cv2.imshow("Template", template)
 
-cv2.imwrite('res.png',img_rgb)
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+found = None
+
+for scale in np.linspace(0.05,2.0,200)[::-1]:
+    resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
+    r = gray.shape[1] / float(resized.shape[1])
+
+    if resized.shape[0] < tH or resized.shape[1] < tW:
+        break
+
+    edged = cv2.Canny(resized, 50, 200)
+    result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+    (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+    # draw a bounding box around the detected region
+    clone = np.dstack([edged, edged, edged])
+    cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+    (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+    cv2.imshow("Visualize", clone)
+    cv2.waitKey(0)
+
+    # if we have found a new maximum correlation value, then update
+    # the bookkeeping variable
+    if found is None or maxVal > found[0]:
+        found = (maxVal, maxLoc, r)
+(_, maxLoc, r) = found
+(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+# draw a bounding box around the detected result and display the image
+cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+cv2.imshow("Image", image)
+cv2.waitKey(0)
